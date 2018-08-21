05df38e8f5165d2b083f34bfb079a0e095b222cd
Theodore Kim <taehyunk@buffalo.edu>
2018-08-21 16:00:21 +0000

    Assignment 6

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -79,31 +79,30 @@ RSpec.describe AddressBook do
         end
     end
         
-    describe "#import_from_csv_2" do
+    describe "#import_from_csv2" do
         it "imports the correct number of entries from 2nd csv file" do
             book.import_from_csv("entries_2.csv")
             book_size = book.entries.size
             
             expect(book_size).to eq 3
     end
-        
+
         it "imports the 1st entry from 2nd csv file" do
             book.import_from_csv("entries_2.csv")
             entry_one = book.entries[0]
-            check_entry(entry_one, "Billybob", "555-555-1234", "billybob@blocmail.com")
+            check_entry(entry_one, "Jose", "555-555-1236", "jose@blocmail.com")
         end
 
         it "imports the 2nd entry from 2nd csv file" do
             book.import_from_csv("entries_2.csv")
             entry_two = book.entries[1]
-            check_entry(entry_two, "Sammy", "111-112-1234", "sammy@blocmail.com")
+            check_entry(entry_two, "Nate", "555-555-1234", "nate@blocmail.com")
         end
         
         it "imports the 3rd entry from 2nd csv file" do
             book.import_from_csv("entries_2.csv")
             entry_three = book.entries[2]
-            check_entry(entry_three, "Joseph", "555-555-1236", "joseph@blocmail.com")
+            check_entry(entry_three, "Sammy", "111-112-1234", "sammy@blocmail.com")
         end
     end
-    
 end
