28e844c62d3110da8d62cb3e470e5c214ba60a64
Theodore Kim <taehyunk@buffalo.edu>
2018-08-27 21:02:32 +0000

    Assignment 7

--- models/address_book.rb
+++ models/address_book.rb
@@ -48,4 +48,17 @@ class AddressBook
             
         return nil
     end
+    
+    def iterative_search(name)
+        index = 0
+        while index < entries.length
+            if name == entries[index].name
+                return entries[index]
+            else
+                index += 1
+            end
+        end
+        
+        return nil
+    end
 end
